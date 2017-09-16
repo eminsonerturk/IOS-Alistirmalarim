6d53bddfd10c3deef14c7823bd62ac6b154269de
eminsonerturk <eminsonerturk@gmail.com>
2017-09-16 23:02:29 +0300

    launch ekleme.

---
+++ LaunchScreenApp/LaunchScreenApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LaunchScreenApp
+//
+//  Created by Emin Soner TÜRK on 16.09.2017.
+//  Copyright © 2017 Emin Soner TÜRK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ LaunchScreenApp/README.md
@@ -0,0 +1,2 @@
+# Açılış Ekranı Ayarlaması
+Burada açılış ekranının logosunun ayarlanması hakkında alıştırma yapılmıştır.
